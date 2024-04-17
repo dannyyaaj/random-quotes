9eaa612b5edc631f1d8c34fcdc5f02321c5d1130
Danny Yang <dannyyaaj@gmail.com>
2024-04-17 08:09:03 -0500

    add quote service and get quote functionality

---
+++ src/app/api/quote.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { Quote } from './quote';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuoteService {
+  private readonly apiPath = 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+
+  constructor(private http: HttpClient) { }
+
+  getQuotes(): Observable<Quote[]> {
+    return this.http.get<Quote[]>(this.apiPath)
+      .pipe(
+        tap((quotes) => console.log('get quotes: ', quotes))
+      )
+  }
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { QuoteService } from '../api/quote.service';
+import { Quote } from '../api/quote';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,29 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy, OnInit {
+  quotes: Quote[] = []
+  subscription: Subscription = new Subscription()
 
+  constructor(private readonly quoteService: QuoteService) { }
+
+  ngOnInit(): void {
+    this.subscription.add(
+      this.getQuotes()
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
+
+  getQuotes(): void {
+    this.quoteService.getQuotes()
+      .subscribe(quotes => this.quotes = quotes)
+  }
+
+  getRandomQuote(quotesData: Quote[]): Quote {
+    const randomIndex = Math.floor(Math.random() * quotesData.length)
+    return quotesData[randomIndex];
+  }
 }
